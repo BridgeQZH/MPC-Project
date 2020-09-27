3c19650916eb3a6d2d410529e4f7ce197b285e0d
BridgeQZH <zju_qzh@163.com>
2020-09-27 16:50:29 +0200

    feat: add the task_MPC file

---
+++ task_MPC.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy
+
+from model import Quadrotor
+from controller import Controller
+from simulation import EmbeddedSimEnvironment
+from mpc import MPC
+
+# Create pendulum and controller objects
+quadrotor = Quadrotor()
+# ctl = Controller()
+
+# Get the system discrete-time dynamics
+A, B, C = quadrotor.get_discrete_system_matrices_at_eq()
+Q = np.diag([1,2,3,4,5,1,1,1,1,1,1,1])
+R = np.diag([1.0/4, 1.0/4, 1.0/4, 1.0/4])
+
+A_np = np.asarray(A)
+B_np = np.asarray(B)
+
+P = np.matrix(scipy.linalg.solve_discrete_are(A_np,B_np,Q,R))
+
+# Instantiate controller
+ctl = MPC(model=quadrotor, 
+        dynamics=quadrotor.discrete_time_dynamics, 
+        Q = Q , R = R, P = P,
+        horizon=7,
+        ulb=-20, uub=20, 
+        xlb=[-np.pi/2, -np.pi/2], 
+        xub=[np.pi/2, np.pi/2],
+        terminal_constraint=None)
+
+# Part II - Simple Inverted Pendulum
+sim_env = EmbeddedSimEnvironment(model=pendulum, 
+                                dynamics=pendulum.discrete_time_dynamics,
+                                controller=ctl.mpc_controller,
+                                time = 6)
+
+t, y, u = sim_env.run([0,0,0,0,0,0,0,0,0,0,0,0])
